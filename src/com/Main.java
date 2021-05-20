0916b6bf217a70fc47fe42dfa4ef4be68a3dbc6e
Sjoerd <sbeetsma99@gmail.com>
2021-05-20 19:28:15 +0200

    Update sorting demonstration main

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,18 +1,40 @@
 package com;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main <E extends Comparable<E>>{
     public static void main(String[] args){
+        Sorter sorter = new Sorter();
         RandomList lst = new RandomList(10, 1);
         ArrayList<Integer> listIntegers = new ArrayList<>(Arrays.asList(3, 2, 6 , 1, 2, 7, 9, 10, 32, 1));
-        System.out.println(listIntegers);
-        System.out.println(lst.getLst());
-        System.out.println(new Sorter().bubbleSort(lst.getLst()));
-        System.out.println(new Sorter().bubbleSort(listIntegers));
+        // sort list of random strings
+        System.out.println("voor sorteren: " + lst.getLst());
+        System.out.println("na sorteren: " + sorter.bubbleSort(lst.getLst(), 1) + "\n");
 
+        // sort list of integers
+        System.out.println("voor sorteren: " + listIntegers);
+        System.out.println(("na sorteren: " + sorter.bubbleSort(listIntegers, 1) + "\n"));
+
+        // simple student object gets ordered by average grade
+        Student student_1 = new Student("Piet");
+        student_1.addGrades(new ArrayList<>(Arrays.asList(9,9,8)));
+        Student student_2 = new Student("Jan");
+        student_2.addGrades(new ArrayList<>(Arrays.asList(3,8,7)));
+        Student student_3 = new Student("Klaas");
+        student_3.addGrades(new ArrayList<>(Arrays.asList(8,6,7)));
+        Student student_4 = new Student("Joep");
+        student_4.addGrades(new ArrayList<>(Arrays.asList(10,10,9)));
+
+
+        // order students
+        ArrayList<Student> students = new ArrayList<>(Arrays.asList(student_1, student_2, student_3, student_4));
+        System.out.println("voor sorteren: " + students);
+        System.out.println("na sorteren: " +sorter.bubbleSort(students, -1) + "\n");
+
+        // Make arraylist of hashmaps
         ArrayList list = new ArrayList();
         HashMap mMap = new HashMap();
 
@@ -34,8 +56,14 @@ public class Main <E extends Comparable<E>>{
         list.add(mMap.clone());
         mMap.clear();
 
-        System.out.println(new Sorter().bubbleSortMaps(list, "Voornaam", 1));
-        System.out.println(new Sorter().bubbleSortMaps(list, "Achternaam", 1));
-        System.out.println(new Sorter().bubbleSortMaps(list, "Leeftijd", 1));
+        // sort by different keys of the map
+        System.out.println("sorteren op Voornaam: "+sorter.bubbleSortMaps(list, "Voornaam", 1));
+        System.out.println("sorteren op Achternaam: "+sorter.bubbleSortMaps(list, "Achternaam", 1));
+        System.out.println("sorteren op leeftijd: "+sorter.bubbleSortMaps(list, "Leeftijd", 1));
+        // reverse sort (high to low)
+        System.out.println("sorteren op leeftijd(reversed): "+sorter.bubbleSortMaps(list, "Leeftijd", -1));
+
+
+
     }
 }
--- src/com/Sorter.java
+++ src/com/Sorter.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Sorter <E extends Comparable<E>>  {
-    public ArrayList<E> bubbleSort(ArrayList<E> lst) {
+    public ArrayList<E> bubbleSort(ArrayList<E> lst, int reverse) {
         for (int i = 0; i < lst.size(); i++) {
             // index j is always i + 1. j iteration becomes shorter because sorted elements build up at end of list. \
             // no point in checking already sorted elements
             for (int j = 1; j < (lst.size() - i); j++) {
-                if (lst.get(j-1).compareTo(lst.get(j)) > 0) {
+                if (lst.get(j-1).compareTo(lst.get(j))*reverse > 0) {
                     //temp var for swap
                     E temp = lst.get(j - 1);
                     lst.set(j - 1, lst.get(j));
@@ -19,7 +19,7 @@ public class Sorter <E extends Comparable<E>>  {
         }
     return lst;
     }
-    // to change to different sorting algorithm for sorting maps
+    // #todo change to different sorting algorithm
     public ArrayList<HashMap> bubbleSortMaps(ArrayList<HashMap> lst, String sortKey, int reverse) {
 
         for (int i = 0; i < lst.size(); i++) {
