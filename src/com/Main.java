42b40bf92270818b3fff95ce2c8de83590d10e2c
Sjoerd <sbeetsma99@gmail.com>
2021-05-20 15:50:00 +0200

    Add Sorter class with bubbleSort method. sorts on String

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,7 +2,8 @@ package com;
 
 public class Main {
     public static void main(String[] args){
-        RandomList lst = new RandomList(10, 10);
+        RandomList lst = new RandomList(5, 4);
         System.out.println(lst.getLst());
+        System.out.println(new Sorter().bubbleSort(lst.getLst()));
     }
 }
