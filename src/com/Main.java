1e4fa1acb8a00287a447198eb9c42e3229c0bead
Sjoerd <sbeetsma99@gmail.com>
2021-05-20 16:12:33 +0200

    Adjust Bubblesort to work with all datatypes as E that have a compareTo method

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,9 +1,15 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        RandomList lst = new RandomList(5, 4);
+        RandomList lst = new RandomList(10, 1);
+        ArrayList<Integer> listIntegers = new ArrayList<>(Arrays.asList(3, 2, 6 , 1, 2, 7, 9, 10, 32, 1));
+        System.out.println(listIntegers);
         System.out.println(lst.getLst());
         System.out.println(new Sorter().bubbleSort(lst.getLst()));
+        System.out.println(new Sorter().bubbleSort(listIntegers));
     }
 }
--- src/com/Sorter.java
+++ src/com/Sorter.java
@@ -3,14 +3,14 @@ package com;
 import java.util.ArrayList;
 
 public class Sorter {
-    public ArrayList<String> bubbleSort(ArrayList<String> lst) {
+    public <E extends Comparable<E>> ArrayList<E> bubbleSort(ArrayList<E> lst) {
         for (int i = 0; i < lst.size(); i++) {
             // index j is always i + 1. j iteration becomes shorter because sorted elements build up at end of list. \
             // no point in checking already sorted elements
             for (int j = 1; j < (lst.size() - i); j++) {
                 if (lst.get(j-1).compareTo(lst.get(j)) > 0) {
                     //temp var for swap
-                    String temp = lst.get(j - 1);
+                    E temp = lst.get(j - 1);
                     lst.set(j - 1, lst.get(j));
                     lst.set(j,temp);
                 }
